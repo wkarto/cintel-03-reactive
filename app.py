a017906fedb5f1ed9306b162ac8abceeaecf9761
wkarto <kwomenker14@gmail.com>
2024-10-27 13:18:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,40 @@
+# Additional Python Notes
+# ------------------------
+
+# Capitalization matters in Python. Python is case-sensitive: min and Min are different.
+# Spelling matters in Python. You must match the spelling of functions and variables exactly.
+# Indentation matters in Python. Indentation is used to define code blocks and must be consistent.
+
+# Functions
+# ---------
+# Functions are used to group code together and make it more readable and reusable.
+# We define custom functions that can be called later in the code.
+# Functions are blocks of logic that can take inputs, perform work, and return outputs.
+
+# Defining Functions
+# ------------------
+# Define a function using the def keyword, followed by the function name, parentheses, and a colon. 
+# The function name should describe what the function does.
+# In the parentheses, specify the inputs needed as arguments the function takes.
+
+# For example:
+#    The function filtered_data() takes no arguments.
+#    The function between(min, max) takes two arguments, a minimum and maximum value.
+#    Arguments can be positional or keyword arguments, labeled with a parameter name.
+
+# The function body is indented (consistently!) after the colon. 
+# Use the return keyword to return a value from a function.
+
+# Calling Functions
+# -----------------
+# Call a function by using its name followed by parentheses and any required arguments.
+    
+# Decorators
+# ----------
+# Use the @ symbol to decorate a function with a decorator.
+# Decorators a concise way of calling a function on a function.
+# We don't typically write decorators, but we often use them.
+
 import plotly.express as px
 from palmerpenguins import load_penguins
 from shiny.express import input, ui, render
